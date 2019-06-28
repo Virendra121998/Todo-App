adafafd717d4a9137f70cb5c327ae2dc8b0d0816
virendra <virendrasingha110@gmail.com>
2019-06-28 12:09:23 +0530

    added test suite for get/todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,11 +1,13 @@
 var request=require('supertest');
 var expect=require('expect');
-
+var {ObjectID}=require('mongodb');
 var {app}=require('./../server');
 var{todo}=require('./../models/Todo');
 const t=[{
+	_id: new ObjectID(),
 	text:"1 test todo"
 },{
+	_id:new ObjectID(),
 	text:"2 test todo"
 }];
 beforeEach((done)=>{
@@ -41,8 +43,31 @@ describe("get /todos",()=>{
 		.get('./todos')
 		.expect(200)
 		.expect((res)=>{
-           expect(res.body.t.length).toBe(2);
+           expect(res.body.docs.length).toBe(2);
 		})
+		.end(done);
+	});
+});
+describe('get /todos/id',()=>{
+	it('should return the todo',(done)=>{
+		request(app)
+		.get(`/todos/${t[0]._id.toHexString()}`)
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.docs.text).toBe(t[0].text);
+		})
+		.end(done);
+	});
+	it('should return 404 ',(done)=>{
+		request(app)
+		.get(`/todos/${t[0]._id}`)
+		.expect(404).
+		end(done);
+	});
+	it('should return 404 for invalid id',(done)=>{
+		request(app)
+		.get('/todos/113').
+		expect(404)
 		.end(done);
 	});
 });
